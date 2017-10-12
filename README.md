d15d3c6546a9f6e53e780de0f2d04f5ba0101989
wanghaihui <conquerwhh@gmail.com>
2017-10-12 15:15:49 +0800

    add http restadapter

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# WhhRetrofit Research -- 样式翻新--专注于网络请求接口的封装
+# WhhRetrofit Research -- 样式翻新 -- 专注于网络请求接口的封装 -- 一个Http请求框架
+
---
+++ app/src/main/java/com/story/retrofit/http/RestAdapter.java
@@ -0,0 +1,10 @@
+package com.story.retrofit.http;
+
+
+
+/**
+ * 将Java方法调用转换成Rest调用
+ */
+@Singleton
+public class RestAdapter {
+}
